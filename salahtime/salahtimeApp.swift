a165c0eed7d6b588289f6ce17d27fc47634f0874
Mohamed Aly <mohamedaly@monzo.com>
2024-04-01 00:29:07 -0400

    Initial Commit

---
+++ salahtime/salahtimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  salahtimeApp.swift
+//  salahtime
+//
+//  Created by Mohamed Aly on 4/1/24.
+//
+
+import SwiftUI
+
+@main
+struct salahtimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
